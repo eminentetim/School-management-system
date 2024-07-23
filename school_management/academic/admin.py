3bc3f7caffe56213b6ccbd87c6f769a5c1f54e42
eminentetim <etimeminent@gmail.com>
2024-07-23 14:54:55 +0100

    Testing

--- school_management/academic/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Curriculum, Lesson, Exam, Assessment, ReportCard
-
-admin.site.register(Curriculum)
-admin.site.register(Lesson)
-admin.site.register(Exam)
-admin.site.register(Assessment)
-admin.site.register(ReportCard)
--- student_information/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Student, Class, Enrollment, Attendance, Grade
-
-admin.site.register(Student)
-admin.site.register(Class)
-admin.site.register(Enrollment)
-admin.site.register(Attendance)
-admin.site.register(Grade)
-
